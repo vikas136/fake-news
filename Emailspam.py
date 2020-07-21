835feba5573a4727258c997c0b0214045737b9a4
vikas136 <68584693+vikas136@users.noreply.github.com>
2020-07-21 15:05:27 +0530

    Add files via upload

---
+++ Emailspam.py
@@ -0,0 +1,116 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox,simpledialog,filedialog
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+nltk.download('all')
+import re
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+root=Tk()
+root.title("Email Spam Classification")
+#root.geometry('800x600')
+root.config(bg="#FAD7A0")
+
+def upload():
+	global train_data
+	train_data=askopenfilename(initialdir="dataset")
+	label=Label(root,text="data is loaded",font='bold')
+	label.grid(row=2,column=0)
+	return train_data
+
+def data():
+	global dframe
+	dframe=pd.read_csv(train_data)
+	label=Label(root,text="data is readed",font='bold')
+	label.grid(row=2,column=2)
+
+def stats():
+	global null
+	null=dframe.isnull().sum()
+	label=Label(root,text=str(null))
+	label.grid(row=4,column=0)
+
+def individuals():
+	global x,y
+	x=dframe['v2']
+	y=dframe['v1'].values
+	return x,y
+
+def encoding():
+	global y
+	lb=LabelEncoder
+	y=lb.fit_transform(y)
+	return y
+
+def grammer():
+	global corpus
+	global x,y
+	global X
+	x,y=individuals()
+	y=encoding()
+	wl=WordNetLemmatizer()
+	cv=CountVectorizer()
+	sw=stopwords.words('english')
+	corpus=[]
+	for i in range(0,len(x)):
+		review=re.sub('[^a-zA-Z]',' ',x[i])
+		review=review.lower()
+		review=review.split()
+		review=[wl.lemmatize(item) for item in review if item not in sw]
+		review=' '.join(review)
+		corpus.append(review)
+	X=cv.fit_transform(corpus).toarray()
+	return X,y
+
+def model_selection():
+	global x_train,x_test,y_train,y_test
+	global X
+	global y
+	X,y=grammer()
+	x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+	return x_train,x_test,y_train,y_test
+
+def Naivebayes():
+	global train_data,dframe
+	global x_train,x_test,y_train,y_test
+	global X
+	global y
+	X,y=grammer()
+	x_train,x_test,y_train,y_test=model_selection()
+	model=MultinomialNB()
+	model.fit(x_train,y_train)
+	predn=model.predict(x_test)
+	cf=confusion_matrix(y_test,predn)
+	accurate=accuracy_score(y_test,predn)
+	a=accurate*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=4,column=2)
+	label=Label(root,text="accuracy: "+str(r),font=15)
+	label.grid(row=5,column=2)
+
+label=Label(root,text="Email Spam Classification",font=('bold',20),fg="red")
+label.grid(row=0,column=1)
+
+myButton=Button(root,text="Upload Dataset",font='bold',command=upload,width=17,bg='orange')
+myButton.grid(row=1,column=0,pady=10)
+myButton=Button(root,text="Data",width=17,command=data,bg='orange',font='bold')
+myButton.grid(row=1,column=2,pady=10)
+myButton=Button(root,text="Null values",width=17,command=stats,bg='orange',font='bold')
+myButton.grid(row=3,column=0,pady=10)
+
+myButton=Button(root,text="MultinomailNB",command=Naivebayes,width=17,bg='orange',font='bold')
+myButton.grid(row=3,column=2,pady=10)
+
+root.mainloop()
---
+++ cancer.py
@@ -0,0 +1,243 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox,simpledialog,filedialog
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+root=Tk()
+root.title("Breast Cancer Prediction")
+#root.geometry('800x600')
+root.config(bg="#FAD7A0")
+
+ 
+def upload():
+	global train_data
+	train_data=askopenfilename(initialdir="dataset")
+	label=Label(root,text="data is loaded",font='bold')
+	label.grid(row=2,column=0)
+	return train_data
+	
+def data():
+	global dframe
+	dframe=pd.read_csv(train_data)
+	label=Label(root,text="data is readed",font='bold')
+	label.grid(row=2,column=1)
+
+def stats():
+	global null
+	null=dframe.isnull().sum()
+	#label=Label(root,text=str(null))
+	#label.grid(row=2,column=2)
+	
+def individuals():
+	global x,y
+	x=dframe.iloc[:,:-1].values
+	y=dframe.iloc[:,-1].values
+	y=y.reshape(-1,1)
+	return x,y
+	
+def model_selection():
+	global x_train,x_test,y_train,y_test
+	x,y=individuals()
+	x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.25,random_state=0)
+	return x_train,x_test,y_train,y_test
+
+def eda():
+	global totals,trains,tests
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	totals=len(dframe)
+	trains=len(x_train)
+	tests=len(x_test)
+	label=Label(root,text=("totalsamples are:"+str(totals),("\ntraining samples are:"+str(trains)),("\n testing samples are:"+str(tests))))
+	label.grid(row=2,column=3)
+'''
+def preprocessing():
+	global ms
+	global x_train,x_test,y_train,y_test
+	x_train,y_train,x_test,y_test=model_selection()
+	ms=MinMaxScaler()
+	x1_train=ms.fit_transform(x_train)
+	x1_test=ms.transform(x_test)
+	return x1_train,x1_test
+'''
+def logistic():
+	global train_data,dframe
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	lr=LogisticRegression()
+	lr.fit(x_train,y_train)
+	predl=lr.predict(x_test)
+	cf=confusion_matrix(y_test,predl)
+	accurate=accuracy_score(y_test,predl)
+	a=accurate*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=5,column=0)
+	label=Label(root,text="accuracy: "+str(r),font=15)
+	label.grid(row=6,column=0)
+	#label=Label(root,text="predictions are"+str(predl))
+	#label.grid(row=7,column=0)
+def random():
+	global train_data,dframe
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	rf=RandomForestClassifier()
+	rf.fit(x_train,y_train)
+	predr=rf.predict(x_test)
+	cf=confusion_matrix(y_test,predr)
+	accurate=accuracy_score(y_test,predr)
+	a=accurate*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=2,column=4,pady=10)
+	label=Label(root,text="accuracy: "+str(r),font=15)
+	label.grid(row=3,column=4,pady=10)
+def knn(): 
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	knn=KNeighborsClassifier()
+	knn.fit(x_train,y_train)
+	predk=knn.predict(x_test)
+	cf=confusion_matrix(y_test,predk)
+	accuratek=accuracy_score(y_test,predk)
+	a=accuratek*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=5,column=1)
+	label=Label(root,text="accuracy: "+str(r),font=15)
+	label.grid(row=6,column=1)
+	#label=Label(root,text="predictions are"+str(predk))
+	#label.grid(row=7,column=1)
+def Decision():
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	dt=DecisionTreeClassifier()
+	dt.fit(x_train,y_train)
+	predd=dt.predict(x_test)
+	cf=confusion_matrix(y_test,predd)
+	accurated=accuracy_score(y_test,predd)
+	a=accurated*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=5,column=2)
+	label=Label(root,text="accuracy: "+str(r),font=15)
+	label.grid(row=6,column=2)
+	#label=Label(root,text="predictions are"+str(predd))
+	#label.grid(row=7,column=2)
+def naivebayes():
+	global nb,predn
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	nb=GaussianNB()
+	nb.fit(x_train,y_train)
+	predn=nb.predict(x_test)
+	cf=confusion_matrix(y_test,predn)
+	accuraten=accuracy_score(y_test,predn)
+	a=accuraten*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=5,column=3)
+	label=Label(root,text="accuracy: "+str(r),font=15)
+	label.grid(row=6,column=3)
+	#label=Label(root,text="predictions are"+str(predn))
+	#label.grid(row=7,column=3)
+def svc():
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	sv=SVC()
+	sv.fit(x_train,y_train)
+	preds=sv.predict(x_test)
+	cf=confusion_matrix(y_test,preds)
+	accurates=accuracy_score(y_test,preds)
+	a=accurates*100
+	r=np.round(a,2)
+	label=Label(root,text="cf: "+str(cf),font=15)
+	label.grid(row=5,column=4)
+	label=Label(root,text="accuracy "+str(r),font=15)
+	label.grid(row=6,column=4)
+	#label=Label(root,text="predictions are"+str(preds))
+	#label.grid(row=7,column=4)
+
+label=Label(root,text="Breast Cancer Prediction",font=('bold',20),fg="red")
+label.grid(row=0,column=2)
+myButton=Button(root,text="Upload Dataset",font='bold',command=upload,width=17,bg='orange')
+myButton.grid(row=1,column=0,pady=10)
+myButton=Button(root,text="Data",width=17,command=data,bg='orange',font='bold')
+myButton.grid(row=1,column=1,pady=10)
+myButton=Button(root,text="Null values",width=17,command=stats,bg='orange',font='bold')
+myButton.grid(row=1,column=2,pady=10)
+myButton=Button(root,text="EDA",width=17,command=eda,bg='orange',font='bold')
+myButton.grid(row=1,column=3,pady=10)
+
+myButton=Button(root,text="RandomForest",width=17,command=random,bg='orange',font='bold')
+myButton.grid(row=1,column=4,pady=10)
+myButton=Button(root,text="Logistic regression",width=17,font='bold',bg="orange",command=logistic)
+myButton.grid(row=4,column=0,pady=10,padx=10)
+myButton=Button(root,text="knn algorithm",width=17,font='bold',bg="orange",command=knn)
+myButton.grid(row=4,column=1,pady=10,padx=10)
+myButton=Button(root,text="Decision Tree",width=17,font='bold',bg="orange",command=Decision)
+myButton.grid(row=4,column=2,pady=10,padx=10)
+myButton=Button(root,text="Naive bayes",width=17,font='bold',bg="orange",command=naivebayes)
+myButton.grid(row=4,column=3,pady=10,padx=10)
+myButton=Button(root,text="SVM",width=17,font='bold',bg="orange",command=svc)
+myButton.grid(row=4,column=4,pady=10,padx=10)
+label=Label(root,text="New Prediction",font=('bold',16),fg='red')
+label.grid(row=8,column=2,padx=10,pady=10)
+
+
+def clear1(event):
+	rad.delete(0,END)
+def clear2(event):
+	tex.delete(0,END)
+def clear3(event):
+	area.delete(0,END)
+def clear4(event):
+	per.delete(0,END)
+def clear5(event):
+	smooth.delete(0,END)
+def predict():
+	global nb,predn
+	global x_train,x_test,y_train,y_test
+	x_train,x_test,y_train,y_test=model_selection()
+	radius=float(rad.get())
+	texture=float(tex.get())
+	marea=float(area.get())
+	perimeter=float(per.get())
+	smoothness=float(smooth.get())
+	nb=GaussianNB()
+	nb.fit(x_train,y_train)
+	predn=nb.predict(x_test)
+	newp=nb.predict([[radius,texture,marea,perimeter,smoothness]])
+	label=Label(root,text="person cancer status: "+str(newp),font=('bold',16),fg='red')
+	label.grid(row=11,column=2,padx=10,pady=10)
+
+rad=Entry(root,width=27,bg="orange")
+rad.grid(row=9,column=0,padx=10,pady=10)
+rad.insert(0,'mean_radius')
+tex=Entry(root,width=27,bg="orange")
+tex.grid(row=9,column=1,padx=10,pady=10)
+tex.insert(0,'mean_texture')
+area=Entry(root,width=27,bg="orange")
+area.grid(row=9,column=2,padx=10,pady=10)
+area.insert(0,'mean_area')
+per=Entry(root,width=27,bg="orange")
+per.grid(row=9,column=3,padx=10,pady=10)
+per.insert(0,'mean_perimeter')
+smooth=Entry(root,width=27,bg="orange")
+smooth.grid(row=9,column=4,padx=10,pady=10)
+smooth.insert(0,'mean_smoothness')
+myButton=Button(root,text="PREDICTION",width=14,font='bold',bg="orange",command=predict)
+myButton.grid(row=10,column=2,pady=10,padx=10)
+root.mainloop()
